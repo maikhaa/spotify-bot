8b07d5a8e8c91b1e63dfa4af1b90bc7b7a20b283
Maikhaa <maikha.abarca@gmail.com>
2022-09-19 08:53:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname});
+});
---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const outputYou = document.querySelector('.output-you');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-GB';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.start();
+});
+
+recognition.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
+});
+
+recognition.addEventListener('result', (e) => {
+  console.log('Result has been detected.');
+
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  outputYou.textContent = text;
+  console.log('Confidence: ' + e.results[0][0].confidence);
+
+  fetch('https://maikha-c7aaatrzsq-ew.a.run.app/predict?text=' + text)
+  .then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  }).catch(function() {
+    console.log("Booo");
+  });
+
+});
+
+recognition.addEventListener('speechend', () => {
+  recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+  console.log('Error: ' + e.error);
+});
