fedfb34431d0ac44429ed21529d42b9770589e40
Maikha Abarca <maikha.abarca@steadypay.co>
2022-09-22 17:43:05 +0100

    Base code for workshop

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,10 @@
 'use strict';
 // Setup Speech Recognition
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-
-recognition.lang = 'en-GB';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
 
 const outputYou = document.querySelector('.output-you');
 document.querySelector('.playlist').style.visibility = "hidden";
 
 // Add event listener - start recognition
-document.querySelector('button').addEventListener('click', () => {
-  recognition.start();
-});
 
 recognition.addEventListener('speechstart', () => {
   console.log('Speech has been detected.');
@@ -23,49 +14,16 @@ recognition.addEventListener('result', (e) => {
   console.log('Result has been detected.');
 
   // Obtain text from result
-  let last = e.results.length - 1;
-  let text = e.results[last][0].transcript;
-
-  outputYou.textContent = text;
-  console.log('Speech recognition confidence: ' + e.results[0][0].confidence);
-
+ 
   // Call emotion detection API
-  const getEmotion = fetch('https://emotion-detection-api-c7aaatrzsq-ew.a.run.app/predict?text=' + text);
 
-  // Log the emotion
   getEmotion
   .then((response) => response.json())
   .then((data) => {
 
-    const emotion = data['label'];
-    console.log('Emotion: ' + emotion);
-
-    const score = data['score'];
-    console.log('Emotion score: ' + score);
+    // Log the emotion
 
     // Display playlists
-    switch(emotion) {
-      case 'joy':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/66F0QrPzMPE9zCj8S1JZ1q?utm_source=generator";
-        break;
-      case 'love':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/64v2LytooaaE1b5ogu0uMo?utm_source=generator";
-        break;
-      case 'anger':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/37i9dQZF1DX08jcQJXDnEQ?utm_source=generator";
-      break;
-      case 'fear':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/31xXPz9MHntnxHvTQ7SYdu?utm_source=generator";
-      break;
-      case 'surprise':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/3CkJH2FzvJciO8UmkzHCMU?utm_source=generator";
-      break;
-      case 'sadness':
-        document.querySelector('.playlist').src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7qK8ma5wgG1?utm_source=generator";
-      break;
-    }
-    
-    document.querySelector('.playlist').style.visibility = "visible";
   
   })
   .catch((error) => {
